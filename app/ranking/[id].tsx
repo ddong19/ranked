9ebd72a9e70b248b8b2e489d590d391563fc95f3
ddong19 <73924020+ddong19@users.noreply.github.com>
2025-08-23 18:49:33 -0400

    Remove unused CSS

--- app/ranking/[id].tsx
+++ app/ranking/[id].tsx
@@ -475,14 +475,6 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     borderRadius: 6,
     zIndex: 1000,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.2,
-    shadowRadius: 2,
-    elevation: 3,
   },
   copyToastText: {
     color: '#fff',
